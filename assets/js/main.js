fc62b5ca91a855a1dac08243db995abc7960bb67
Dipankar <69777055+csdip@users.noreply.github.com>
2025-07-26 12:44:56 +0530

    upload everything

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,31 @@
-// let day = document.querySelector(".days");
-let hour = document.querySelector(".hours");
-let min = document.querySelector(".min");
-let sec = document.querySelector(".sec");
-let ampm = document.querySelector(".am-pm");
+const day = document.querySelector(".days");
+const hour = document.querySelector(".hours");
+const min = document.querySelector(".min");
+const sec = document.querySelector(".sec");
+const ampm = document.querySelector(".am-pm");
 
-const updateClock =()=> {
-  // let d = new Date().getDate();
+function updateClock() {
+  let d = new Date().getDate()
   let h = new Date().getHours();
   let m = new Date().getMinutes();
   let s = new Date().getSeconds();
+  let ampm = "am";
 
   if (h > 12) {
     h = h - 12;
     ampm = "pm";
   }
 
+  d = d < 10 ? "0" + d : d;
   h = h < 10 ? "0" + h : h;
   m = m < 10 ? "0" + m : m;
   s = s < 10 ? "0" + s : s;
 
-  // day.innerText = d;
+  day.innerText = d;
   hour.innerText = h;
   min.innerText = m;
   sec.innerText = s;
-  ampm.innerText = am;
+  ampm.innerText = ampm;
 
 
   setTimeout(() => {
@@ -33,4 +35,5 @@ const updateClock =()=> {
 };
 
 updateClock();
+
 
